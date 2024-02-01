921f8ac8a9133274ab76ddc0af1ef7fa6fda58f5
KatterMaw <narcat99@gmail.com>
2024-02-01 18:08:05 +0500

    Initial

---
+++ TestTheme/Assists/ButtonAssist.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace TestTheme.Assists;
+
+internal sealed class ButtonAssist
+{
+	public static readonly AvaloniaProperty<BoxShadows> BoxShadowProperty =
+		AvaloniaProperty.RegisterAttached<Button, BoxShadows>("BoxShadow", typeof(ButtonAssist));
+	public static readonly AvaloniaProperty<IBrush?> HoverBackgroundProperty =
+		AvaloniaProperty.RegisterAttached<Button, IBrush?>("HoverBackground", typeof(ButtonAssist));
+	public static readonly AvaloniaProperty<IBrush?> PressedBackgroundProperty =
+		AvaloniaProperty.RegisterAttached<Button, IBrush?>("PressedBackground", typeof(ButtonAssist));
+}
---
+++ TestTheme/DefaultTheme.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace TestTheme;
+
+public sealed class DefaultTheme : Styles
+{
+	public DefaultTheme(IServiceProvider? serviceProvider = null)
+	{
+		AvaloniaXamlLoader.Load(serviceProvider, this);
+	}
+}
